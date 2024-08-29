e3ba1684488af77c9f35d58b46e739fffe89c95e
shaheenj <jawadishaheen@gmail.com>
2024-08-29 21:41:13 +0100

    feat: Add migration for creating kanban_tasks table

---
+++ database/migrations/2024_08_29_203924_create_kanban_tasks.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kanban_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('column_id');
+            $table->string('title');
+            $table->json('tags'); // Store tags as JSON
+            $table->timestamps();
+            $table->unsignedBigInteger('assigned_by')->nullable(); 
+            $table->unsignedBigInteger('assigned_to')->nullable(); 
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kanban_tasks');
+    }
+};
