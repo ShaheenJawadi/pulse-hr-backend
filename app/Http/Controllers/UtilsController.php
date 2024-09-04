545d5cbd2285cf79794a903af2214a52ab65ee20
shaheenj <jawadishaheen@gmail.com>
2024-09-04 18:41:45 +0100

    feat: Add kanbanLister method to UtilsController

--- app/Http/Controllers/UtilsController.php
+++ app/Http/Controllers/UtilsController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KanbanColumn;
 use Illuminate\Http\Request;
+use App\Utils\ApiResponse;
 
 class UtilsController extends Controller
 
 
 {
-    
+
+    public function kanbanLister()
+    {
+        $kanbanData = KanbanColumn::with(['tasks'])->get();
 
 
+        return ApiResponse::success($kanbanData, 'success ');
+    }
 }
