5e3077a080d1ab67c7f269e7240cff150e2febde
shaheenj <jawadishaheen@gmail.com>
2024-09-04 18:40:52 +0100

    refactor: Update foreign key name in kanban_task_tag migration

--- database/migrations/2024_09_04_170534_kanban_task_tag.php
+++ database/migrations/2024_09_04_170534_kanban_task_tag.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('kanban_task_tag', function (Blueprint $table) {
             $table->id();
             $table->foreignId('kanban_task_id')->constrained('kanban_tasks')->onDelete('cascade');
-            $table->foreignId('kanban_tag_id')->constrained('kanban_tags')->onDelete('cascade');
+            $table->foreignId('kanban_tags_id')->constrained('kanban_tags')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('kanban_task_tag');
+         Schema::dropIfExists('kanban_task_tag');
     }
 };
